dbe5f45ddd1cff66fe812af0760beb1a7593c5c9
Crola1702 <cristobal.arroyo@ekumenlabs.com>
2025-11-06 21:44:24 -0500

    Use base_url correctly
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,9 +1,9 @@
-def parse_markdown_link(link_str, format=:link, html_classes="")
+def parse_markdown_link(link_str, format=:link, html_classes="", img_base_url="")
   regex_match = /\[(.*)\]\((.*)\)/.match link_str
   case format
   when :link
     "<a class='#{html_classes}' href='#{regex_match[2]}' alt='#{regex_match[1]}'>#{regex_match[1]}</a>"
   when :image
-    "<img class='#{html_classes}' src='#{regex_match[2]}' alt='#{regex_match[1]}'>"
+    "<img class='#{html_classes}' src='#{img_base_url}#{regex_match[2]}' alt='#{regex_match[1]}'>"
   end
 end
